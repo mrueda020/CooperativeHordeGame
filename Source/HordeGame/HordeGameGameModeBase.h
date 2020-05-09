6ff881253b65fffea164130376ed813fdb31a203
mrueda020 <mrueda020@hotmail.com>
2020-05-09 18:30:17 -0500

    Initial Submit

---
+++ Source/HordeGame/HordeGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HordeGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORDEGAME_API AHordeGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
